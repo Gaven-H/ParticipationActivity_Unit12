303ebf9e093401f25fa8c0cf1d996f1f4a6990de
Gaven <ghuynh@student.cscc.edu>
2026-04-12 19:34:16 -0400

    Define settings for the game (Screen name, screen width and height, screen fps)

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
 class Settings:
     def __init__(self) -> None:
-        
+        self.name: str = "Alien Invasion"
+        self.screen_w = 1200
+        self.screen_h = 800
+        self.FPS = 60
