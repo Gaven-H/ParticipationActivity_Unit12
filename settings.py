932a5132f598cbe78fd78a4c364e5e5312fb86e8
Gaven <ghuynh@student.cscc.edu>
2026-04-12 19:58:53 -0400

    Set image for ship

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+
+        self.ship_file = Path.cwd() / 'Aessets' / 'images' / 'ship2(no bg).png'
