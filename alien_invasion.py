6c3562a63ce2e27217919d52d04f43b3044b6170
Gaven <ghuynh@student.cscc.edu>
2026-04-12 19:18:54 -0400

    run game = true on __init__

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,11 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200.800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.running = True
+
     def run_game(self):
         # Game Loop
-        
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
