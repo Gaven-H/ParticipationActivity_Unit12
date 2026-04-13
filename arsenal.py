1308750313d74048a119b9cd5b3d94c2442306cb
Gaven <ghuynh@student.cscc.edu>
2026-04-12 22:52:27 -0400

    Define remove bullets offscreen in Arsenal class

--- arsenal.py
+++ arsenal.py
@@ -15,11 +15,13 @@ class Arsenal:
     def update_arsenal (self) -> None:
         self.arsenal.update()
 
+    def _remove_bullets_offscreen(self) -> None:
+
     def draw(self) -> None:
         for bullet in self.arsenal:
             bullet.draw_bullet()
         
-    def fire_bullet(self):
+    def fire_bullet(self) -> bool:
         if len(self.arsenal) < self.settings.bullet_amount:
             new_bullet = Bullet(self.game)
             self.arsenal.add(new_bullet)
